3a8653f624469d15d86fd8dfcc3505924045ab64
yeshwanth8875 <82574261+yeshwanth8875@users.noreply.github.com>
2021-04-29 22:02:44 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -77,7 +77,7 @@ And the following:
     dispatcher.bot.first_name, ""
     if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-SAITAMA_IMG = "https://telegra.ph/file/dc23351053dbb093f5076.jpg"
+SAITAMA_IMG = "https://telegra.ph/file/4ef8896a0a0ed55e779d0.jpg"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
 Senku is hosted on one of Heroku's Servers and doesn't require any donations as of now but \
